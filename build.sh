1820b6325ffb14c7960a619790c910cfd19f9503
FoxiooOfficial <foxioo2137@gmail.com>
2026-07-08 17:17:00 +0200

    again fixed...? please work...

--- build.sh
+++ build.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+cd /content
+source build/envsetup.sh
+lunch cm_e400-userdebug
+
 make bacon -j$(nproc)
--- prebuild.sh
+++ prebuild.sh
@@ -3,7 +3,3 @@
 apt-get update && apt-get install -y build-essential lib32ncurses5-dev make python git curl gnupg bison flex gperf libsdl1.2-dev libesd0-dev libwxgtk2.8-dev squashfs-tools zip zlib1g-dev libncurses5-dev g++-multilib lib32z1-dev openjdk-6-jdk wget
 
 ln -sf /usr/include/x86_64-linux-gnu/zconf.h /usr/include/zconf.h
-
-cd /content
-source build/envsetup.sh
-lunch cm_e400-userdebug
