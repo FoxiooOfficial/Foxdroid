f18cf169c2edcde83a4efc27aebfa09e4ce410b8
FoxiooOfficial <foxioo2137@gmail.com>
2026-07-09 01:56:59 +0200

    for the future: NTFS is EVILLLLLLL and could ruin all progress if some silly fox stores temporary files on windows instend of WSL and copy pasting files from here to here- UGDDFJIEJIH :3

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 cd /content
+find . -name "*:Zone.Identifier" -delete
+
+if [ "$1" == "clean" ]; then
+    make installclean
+fi
+
 source build/envsetup.sh
 lunch cm_e400-userdebug
 
