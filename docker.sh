6cd96bcce75ec469199d4849e2c4400e270920f2
FoxiooOfficial <foxioo2137@gmail.com>
2026-07-08 17:03:30 +0200

    fixed/added scripts

--- docker.sh
+++ docker.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-sudo docker run -it -v $(pwd):/content ubuntu:14.04 /bin/bash
+CONTAINER_NAME="foxdroid-build"
+
+if sudo docker ps -a --format '{{.Names}}' | grep -q "^${CONTAINER_NAME}$"; then
+    echo "Loading container ^${CONTAINER_NAME}$..."
+    sudo docker start -ai "$CONTAINER_NAME"
+else
+    echo "Creating new ^${CONTAINER_NAME}$ container..."
+    sudo docker run -it --name "$CONTAINER_NAME" -v $(pwd):/content ubuntu:14.04 /bin/bash
+fi
